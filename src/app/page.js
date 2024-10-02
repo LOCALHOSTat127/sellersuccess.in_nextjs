d2463ce17a73e7fed47f0c82c9d3375e4b7f85be
SAHIL JOSHI <125807226+LOCALHOSTat127@users.noreply.github.com>
2024-10-02 20:05:40 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -201,7 +201,7 @@ const fetchFeaturedBlogs = async () => {
   let featuredBlogs = null;
   try {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/blogs/recent_blogs`,
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}api/blogs/recent_blogs`,
       {
         method: "POST",
         headers: {
